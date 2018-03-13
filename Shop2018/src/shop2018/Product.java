69e56e270e39c60b7521e1329efc454e934f8d1d
Student <Student@193.107.34.203>
2018-03-13 13:54:21 +0100

    Product

---
+++ Shop2018/src/shop2018/Product.java
@@ -0,0 +1,40 @@
+package shop2018;
+
+import java.math.BigDecimal;
+
+public class Product  implements Comparable<Product>{
+    private String name;
+    private BigDecimal price;
+    private int count = 100;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+    @Override
+    public String toString()
+    {
+        return name +"("+price+"zł, "+count+"szt.)";
+    }
+    public int getCount() {
+        return count;
+    }
+    public void setCount (int count){
+        this.count = count;
+    }
+    @Override
+    public int compareTo(Product second) {
+        return name.compareTo(second.name);
+    }
+    
+    
+    
+}
