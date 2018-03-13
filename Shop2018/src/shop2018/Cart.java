3b973db04ffdc7f8f7f26b43556c6ee900a5732f
Student <Student@193.107.34.203>
2018-03-13 13:53:52 +0100

    cart

---
+++ Shop2018/src/shop2018/Cart.java
@@ -0,0 +1,48 @@
+package shop2018;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import shop2018.Product;
+
+
+public class Cart {
+    //private List<Produkt> lista = new ArrayList();
+    private Map<Product,Integer> map=new HashMap();
+    public void add(Product p)
+    {
+        if(map.containsKey(p))
+        {
+            int i=map.get(p);
+            i++;
+            map.put(p, i);
+          
+        }
+        else
+        {
+            
+        map.put(p,1);
+    }}
+    
+    public BigDecimal toPay()
+    {
+        BigDecimal sum = new BigDecimal(0);
+        for(Product p : map.keySet())
+        {
+            int ilosc=map.get(p);
+            sum=sum.
+                    add(p.getPrice()
+                    .multiply(new BigDecimal(ilosc)));
+            
+        }
+        return sum;
+    }
+    public String toString()
+    {
+        return map.toString()+"Razem do Zapłaty:"+toPay();
+    }
+    
+    
+}
